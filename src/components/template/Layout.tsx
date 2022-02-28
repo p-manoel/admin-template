c30455cb05169b54c272559272c767bfc03ff91a
Pedro Manoel <dev.pedromanoel@pm.me>
2022-02-28 17:32:20 -0300

    chore(style): set theme configs

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,12 +10,20 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   return (
-    <div>
+    <div className={`flex h-screen w-screen`}>
       <SideBar />
-      <TopBar title={props.title} subtitle={props.subtitle} />
-      <Content>
-        {props.children}
-      </Content>
+      <div className={`
+        flex
+        flex-col
+        w-full
+        p-7
+        bg-gray-300 dark:bg-gray-800
+      `}>
+        <TopBar title={props.title} subtitle={props.subtitle} />
+        <Content>
+          {props.children}
+        </Content>
+      </div>
     </div>
   )
 }
